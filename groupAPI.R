5fb8c533c3ec884985d86be95d2d9fe9ade49ea5
andreitmunteanu1978 <andreiteodor.munteanu@rompetrol.com>
2025-03-07 23:52:50 +0200

    Update groupAPI.R

--- groupAPI.R
+++ groupAPI.R
@@ -5,44 +5,13 @@ library(dplyr)
 library(jsonlite)
 library(base64enc)
 
-#* @post /structure
-#* @param File:file
-#* @serializer json
-function(req) {
-  
-  # Create variable to store the binary file data 
-  file_binary <- req$body$File$value
-  
-  # Check if a file is uploaded
-  if (is.null(file_binary) || length(file_binary)==0) {
-    return(list(error = "No file uploaded"))
-  }
-  
-  # Create a temporary directory to store the file
-  temp_file <- tempfile(fileext = ".xlsx")
-  
-  # Save the file
-  writeBin(file_binary, temp_file)
-  
-  # Import the data frame
-  df <- data.frame(read_excel(temp_file))
-  
-  # Delete the temporary file
-  unlink(temp_file)
-  
-  response <- data.frame(matrix(data=cbind(names(df[1,]),t(df[1,])),ncol = 2, nrow = nrow(t(df[1,])), dimnames = list(NULL,c("Key","Value"))))
-  
-  # Return the results JSON
-  return(response)
-}
-
 #* @post /data
 #* @param File:file
 #* @serializer json
 function(req) {
   
   # Create variable to store the binary file data 
-  file_binary <- req$body$File$value
+  file_binary <- req$body$file$value
 
   # Check if a file is uploaded
   if (is.null(file_binary) || length(file_binary)==0) {
@@ -57,11 +26,11 @@ function(req) {
   
   # Import the data frame
   df <- data.frame(read_excel(temp_file))
-  
+
   # Retrieve parameters from request body
-  group_by_columns <- as.vector(unlist(req$body$group_by_columns))
-  aggregation_columns <- as.vector(unlist(req$body$aggregation_columns))
-  aggregation_method <- as.vector(unlist(req$body$aggregation_method))
+  group_by_columns <- as.vector(unlist(fromJSON(rawToChar(req$body$group_by_columns$value))))
+  aggregation_columns <- as.vector(unlist(fromJSON(rawToChar(req$body$aggregation_columns$value))))
+  aggregation_method <- as.vector(unlist(fromJSON(rawToChar(req$body$aggregation_method$value))))
   
   # Validate group_by_columns
   if (is.null(group_by_columns) || !all(group_by_columns %in% colnames(df))) {
@@ -89,4 +58,35 @@ function(req) {
   # Return the results JSON
   return(response)
 
+}
+
+#* @post /structure
+#* @param File:file
+#* @serializer json
+function(req) {
+  
+  # Create variable to store the binary file data 
+  file_binary <- req$body$file$value
+  
+  # Check if a file is uploaded
+  if (is.null(file_binary) || length(file_binary)==0) {
+    return(list(error = "No file uploaded"))
+  }
+  
+  # Create a temporary directory to store the file
+  temp_file <- tempfile(fileext = ".xlsx")
+  
+  # Save the file
+  writeBin(file_binary, temp_file)
+  
+  # Import the data frame
+  df <- data.frame(read_excel(temp_file))
+
+  # Delete the temporary file
+  unlink(temp_file)
+  
+  response <- data.frame(matrix(data=cbind(names(df[1,]),t(df[1,])),ncol = 2, nrow = nrow(t(df[1,])), dimnames = list(NULL,c("Key","Value"))))
+  
+  # Return the results JSON
+  return(response)
 }
