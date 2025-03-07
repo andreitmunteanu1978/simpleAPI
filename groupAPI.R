082293f6d0f8beeb022bc4c481a0ce790e6942b2
andreitmunteanu1978 <andreiteodor.munteanu@rompetrol.com>
2025-03-08 01:41:28 +0200

    Update groupAPI.R

--- groupAPI.R
+++ groupAPI.R
@@ -32,9 +32,9 @@ function(req) {
   
 
   # Retrieve parameters from request body
-  group_by_columns <- as.vector(unlist(fromJSON(rawToChar(req$body$group_by_columns$value))))
-  aggregation_columns <- as.vector(unlist(fromJSON(rawToChar(req$body$aggregation_columns$value))))
-  aggregation_method <- as.vector(unlist(fromJSON(rawToChar(req$body$aggregation_method$value))))
+  group_by_columns <- as.vector(unlist(req$body$group_by_columns$value))
+  aggregation_columns <- as.vector(unlist(req$body$aggregation_columns$value))
+  aggregation_method <- as.vector(unlist(req$body$aggregation_method$value))
   
   # Validate group_by_columns
   if (is.null(group_by_columns) || !all(group_by_columns %in% colnames(df))) {
