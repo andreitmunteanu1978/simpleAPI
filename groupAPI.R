dd1fae12f462f54f982c241e0b449562fd7ac405
andreitmunteanu1978 <andreiteodor.munteanu@rompetrol.com>
2025-03-08 01:52:59 +0200

    Update groupAPI.R

--- groupAPI.R
+++ groupAPI.R
@@ -29,7 +29,10 @@ function(req) {
   # Import the data frame
   df <- data.frame(read_excel(temp_file))
   print(nrow(df))
-  
+
+  print(req$body$group_by_columns)
+  print(rawToChar(req$body$group_by_columns))
+  print(fromJSON(rawToChar(req$body$group_by_columns)))
 
   # Retrieve parameters from request body
   group_by_columns <- as.vector(unlist(fromJSON(rawToChar(req$body$group_by_columns))))
