231cf1e93de7692ed13a93fbb88061d756faa0bb
andreitmunteanu1978 <andreiteodor.munteanu@rompetrol.com>
2025-03-14 19:11:41 +0200

    Update groupAPI.R

--- groupAPI.R
+++ groupAPI.R
@@ -5,71 +5,117 @@ library(dplyr)
 library(jsonlite)
 library(base64enc)
 
-f_ippa <- function(binary_file, date) {
+# Create all applicable functions for the API POST action
   
-  # Create a temporary directory to store the file
-  temp_file <- tempfile(fileext = ".xlsx")
+  # Declare the function for processing the Livrari Depozite.xlsx file
+  f_shipments_depots <- NULL
   
-  # Save the file
-  writeBin(base64decode(binary_file), temp_file)
+  # Declare the function for processing the Avize Transfer VEGA.xlsx file
+  f_shipments_vega <- NULL
   
-  # Import the data frame
-  df <- data.frame(read_excel(temp_file))
-  
-  # Delete the temporary file
-  unlink(temp_file)
-
-  # Select the grouping columns
-  df <- df[,colnames(df) %in% c("Name.of.the.ship.to.party","Deliv..date.From.to.","Description","Delivery.quantity")]
-  colnames(df) <- c("CUSTOMER","DATE","PRODUCT","QUANTITY")
-  aggregation_columns <- c("QUANTITY")
-
-  # Format the 'DATE' column
-  df$DATE <- as.Date(df$DATE)
-  
-  # Filter the data by latest +/- 10 days
-  df <- df[format(as.Date(df$DATE),"%Y-%m-%d") %in% as.Date(date+seq(-5,5)),]
-  
-  # Group the dataset
-  #response <- df %>% group_by(across(all_of(colnames(df)[!colnames(df) %in% aggregation_columns]))) %>% summarise(across(all_of(aggregation_columns), sum, na.rm = TRUE), .groups = "drop")
-  # Group the dataset
-  DS1 <- df %>% group_by(across(all_of(colnames(df)[!colnames(df) %in% c(aggregation_columns)]))) %>% summarise(across(all_of(aggregation_columns), sum, na.rm = TRUE), .groups = "drop")
-  DS2 <- df %>% group_by(across(all_of(colnames(df)[!colnames(df) %in% c(aggregation_columns,"CUSTOMER")]))) %>% summarise(across(all_of(aggregation_columns), sum, na.rm = TRUE), .groups = "drop")
-
-  # Define the array of group_by_columns
-  group_by_colums <- colnames(df)[!colnames(df) %in% aggregation_columns]
-  
-  DataList <- list(
-    list(Table="Customer/Date/Product", Data = DS1, Groups = group_by_colums),
-    list(Table="Date/Product", Data = DS2, Groups = group_by_colums)
-  )
-  
-  # Return the results JSON
-  return(fromJSON(toJSON(DataList,pretty=TRUE, auto_unbox = TRUE)))
+  # Declare function for processing the IPPA.xlsx file
+  f_ippa <- function(bin_file, ref_date, ref_range) {
+    
+    # Create a temporary directory to store the file
+    temp_file <- tempfile(fileext = ".xlsx")
+    
+    # Save the file
+    writeBin(base64decode(bin_file), temp_file)
+    
+    # Import the Excel data into a data.frame
+    df <- data.frame(read_excel(temp_file))
+    
+    # Delete the temporary file
+    unlink(temp_file)
+    
+    # Select the grouping columns
+    df <- df[,colnames(df) %in% c("Name.of.the.ship.to.party","Deliv..date.From.to.","Description","Delivery.quantity")]
+    colnames(df) <- c("CUSTOMER","DATE","PRODUCT","QUANTITY")
+    aggregation_columns <- c("QUANTITY")
+    
+    # Format the 'DATE' column using the as.Date formula
+    df$DATE <- as.Date(df$DATE)
+    
+    # Filter the data by latest +/- 10 days
+    df <- df[format(as.Date(df$DATE),"%Y-%m-%d") %in% as.Date(ref_date+seq(-ref_range,ref_range)),]
+    
+    # Create 2 response data.frames
+    df_1 <- df %>% group_by(across(all_of(colnames(df)[!colnames(df) %in% c(aggregation_columns)]))) %>% summarise(across(all_of(aggregation_columns), sum, na.rm = TRUE), .groups = "drop")
+    df_2 <- df %>% group_by(across(all_of(colnames(df)[!colnames(df) %in% c(aggregation_columns, "CUSTOMER")]))) %>% summarise(across(all_of(aggregation_columns), sum, na.rm = TRUE), .groups = "drop")
+    
+    # Define the array of group_by_columns
+    group_by_colums <- colnames(df)[!colnames(df) %in% aggregation_columns]
+    
+    # Store the data.frames into a list
+    DataList <- list(
+      list(Table="Customer/Date/Product", Data = df_1, Groups = group_by_colums),
+      list(Table="Date/Product", Data = df_2, Groups = group_by_colums)
+    )
+    
+    # Return the results JSON
+    return(fromJSON(toJSON(DataList, pretty=TRUE, auto_unbox = TRUE)))
   }
+  
+  # Declare the function for processing the JET.xlsx file
+  f_jet <- NULL
+  
+  # Declare the function for processing the Program vag si vapoare.xlsx file
+  f_shipments_trains_barges <- NULL
+  
+  # Declare the function for processing the LIVRARI.xlsx file
+  f_shipments_others <- NULL
+  
+  # Declare the function for processing the Stoc.xlsx file
+  f_stocks <- NULL
 
+#_______________________________________________________________
+  
 #* @post data
 #* @param File:file
 #* @serializer json
 function(req) {
   
-  # Define the file category
+  # Import all POST body components and verify their accuracy/existance
+  
+  #1:  Define the file category
   file_category <- req$body$category
   
-  # Define the reference date
-  refDate <- as.Date(req$body$refdate)
+  # Check if a file category exists
+  if (is.null(file_category) || length(file_category)==0) {
+    return(list(error = "You have not selected a file processing method."))
+  }
+  
+  #2. Define the reference date
+  refDate <- as.Date(req$body$refDate)
+  
+  if (is.null(refDate) || length(refDate)==0) {
+    return(list(error = "You have not choosen a reference date."))
+  }
+  
+  #3. Define the reference range
+  refRange <- as.Date(req$body$refRange)
+  
+  if (is.null(refRange) || length(refRange)==0) {
+    return(list(error = "You have not choosen a valid reference range for your reference date."))
+  }
   
   # Create variable to store the binary file data 
   file_binary <- req$body$File
-
+  
   # Check if a file is uploaded
   if (is.null(file_binary) || length(file_binary)==0) {
     return(list(error = "No file uploaded"))
   }
   
   switch(file_category,
-         "IPPA" = f_ippa(file_binary, refDate)
-         )
+         "Shipments and Depots" = f_shipments_depots(file_binary, refDate, refRange),
+         "Shipments to Vega" = f_shipments_vega(file_binary, refDate, refRange),
+         "IPPA" = f_ippa(file_binary, refDate, refRange),
+         "Jet" = f_jet(file_binary, refDate, refRange),
+         "Trains and Barges" = f_shipments_trains_barges(file_binary, refDate, refRange),
+         "Shipments and Others" = f_shipments_others(file_binary, refDate, refRange),
+         "Stocks" = f_stocks(file_binary, refDate, refRange),
+  )
 }
 
 #_______________________________________________________________
@@ -81,7 +127,7 @@ function(req) {
   
   # Create variable to store the binary file data 
   file_binary <- req$body$File
-
+  
   # Check if a file is uploaded
   if (is.null(file_binary) || length(file_binary)==0) {
     return(list(error = "No file uploaded"))
@@ -95,7 +141,7 @@ function(req) {
   
   # Import the data frame
   df <- data.frame(read_excel(temp_file))
-
+  
   # Retrieve parameters from request body
   group_by_columns <- as.vector(unlist(req$body$group_by_columns))
   aggregation_columns <- as.vector(unlist(req$body$aggregation_columns))
@@ -123,10 +169,10 @@ function(req) {
   unlink(temp_file)
   
   response <- df %>% group_by(across(all_of(group_by_columns))) %>% summarise(across(all_of(aggregation_columns), agg_func, na.rm = TRUE), .groups = "drop")
-
+  
   # Return the results JSON
   return(response)
-
+  
 }
 
 #_______________________________________________________________
@@ -135,7 +181,7 @@ function(req) {
 #* @param File:file
 #* @serializer json
 function(req) {
-
+  
   print(file$body$category)
   
   # Create variable to store the binary file data 
@@ -154,7 +200,7 @@ function(req) {
   
   # Import the data frame
   df <- data.frame(read_excel(temp_file))
-
+  
   # Delete the temporary file
   unlink(temp_file)
   
@@ -163,3 +209,4 @@ function(req) {
   # Return the results JSON
   return(response)
 }
+
